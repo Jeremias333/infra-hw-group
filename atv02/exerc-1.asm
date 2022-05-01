a2d585364bc6e6696190a81a23024cb9951aae26
JacquelinBusch <79542086+JacquelinBB@users.noreply.github.com>
2022-05-01 00:03:06 -0300

    Update exerc-1.asm

--- atv02/exerc-1.asm
+++ atv02/exerc-1.asm
@@ -8,30 +8,50 @@
 	
 	#A=$s1  B=$s2  C=$s3  D=$s4
 .text
-	addi $v0, $0, 5 #v0 recebe valor do usuario de A
-	add $s1, $0, $v0 #s1 salva o valor do usuario de A
-	addi $v0, $0, 5 #v0 recebe valor do usuario de B
-	add $s2, $0, $v0 #s2 salva o valor do usuario de B
-	addi $v0, $0, 5 #v0 recebe valor do usuario de C
-	add $s3, $0, $v0 #s3 salva o valor do usuario de C
-	addi $v0, $0, 5 #v0 recebe valor do usuario de D
-	add $s4, $0, $v0 #s4 salva o valor do usuario de D
+	# Pegar input (A) do usuário
+	li $v0, 5
+	syscall
 	
+	# Salvar o resultado em $s1
+	move $s1, $v0
+	
+	# Pegar input (B) do usuário
+	li $v0, 5
+	syscall
+	
+	# Salvar o resultado em $s2
+	move $s2, $v0
+	
+	# Pegar input (C) do usuário
+	li $v0, 5
+	syscall
+	
+	# Salvar o resultado em $s3
+	move $s3, $v0
+	
+	# Pegar input (D) do usuário
+	li $v0, 5
+	syscall
+	
+	# Salvar o resultado em $s4
+	move $s4, $v0
+	
+	#Calculo
 	add $t0, $s1, $s4 #soma A+D e coloca o resultado num registrador temporario $t0
 	lw $t1, num0 #recebe 7 em $t3
 	lw $t2, num1 #recebe 2 em $t4
 	lw $t3, num2 #recebe 5 em $t5
 	beq $t0, $t1, L1 #se A+D==7 vai para LABEL1
 	beq $s2, $t2, L1 #se B==2 vai para LABEL1
-	bne $s2, $t2, Else #caso argumento anterior seja falso vai para Else
+	bne $s2, $t2, Else #caso argumento anterior seja falso vai para o Else
 	
 	L1: bne $s3, $t3, L2 #se C!=5 vai para LABEL2
-	beq $s3, $t5, Else #caso argumento anterior seja falso vai para Else
+	beq $s3, $t5, Else #caso argumento anterior seja falso vai para o Else
 	
-	L2: addi $s0, $0, 1
-	la $a0, msg1 #carrega o endereco base da string em  $t2
-	syscall #Imprime "TRUE"
+	L2: li $v0, 4
+	la $a0, msg1 #Imprime "TRUE"
+	syscall
 	
-	Else: addi $s0, $0, 1
-	la $a0, msg2 #carrega o endereco base da string em  $t1
-	syscall #Imprime "FALSE"
+	Else: li $v0, 4
+	la $a0, msg2 #Imprime "FALSE"
+	syscall
