79fe1434ee665a93f1ce62734cbf7850f46020df
Elder Lamarck <elder.lamarck@gmail.com>
2022-05-03 08:47:59 -0300

    Update exerc-3

--- atv02/exerc-3.asm
+++ atv02/exerc-3.asm
@@ -1,12 +1,10 @@
 .data
-	numero: .word 1
 	virgula: .asciiz ", "
 	newline: .asciiz "\n"
 	
 .text
 main:
 	#receber um input e salvar em t0
-	lw $s1 numero
 	addi $v0, $0, 5 #v0 recebe valor do usuario
 	syscall
 	add $s0, $0,$v0 #s0 salva o valor do usuario
@@ -14,7 +12,7 @@ main:
 	
 Print:
 	#print input
-	add $t0, $t0, $s1 #t0++
+	addi $t0, $t0, 1 #t0++
 	addi $v0, $0, 1
 	add $a0, $0, $t0
 	syscall
@@ -33,11 +31,11 @@ Print:
 
 subOne:
 	#verificando se t0 é maior ou igual a 1
-	beq $s0, $s1, exit
+	beq $s0, 1, exit
 	
 	#resetando t0 e subtraindo 1 do valor do usuario
 	add $t0, $0, $0
-	sub $s0, $s0, $s1
+	subi $s0, $s0, 1
 	
 	#printando nova linha
 	addi $v0, $0, 4
