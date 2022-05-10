02129af02f59bb3b3cfb7696a90d5bc44c730c26
JacquelinBusch <79542086+JacquelinBB@users.noreply.github.com>
2022-05-10 17:30:06 -0300

    Update exerc-4.asm

--- atv02/exerc-4.asm
+++ atv02/exerc-4.asm
@@ -1,3 +1,14 @@
+#################################################################################################################################################################
+#Nome do grupo: LearnWARE 
+#Questão de número: 4
+#Atividade de número: 2
+#Disciplina: Infraestrutura de Hardware
+#Professor: Vitor Coutinho
+#Semestre Letivo: 3º semestre
+#Turma: B
+#Alunos: Diego Moura, Elder Lamarck, Jacquelin Busch, Jeremias Oliveira
+#Objetivo do algoritmo: Vai pegar um input inteiro do usuário em segundos e transformar isso em um formato de anos, meses, dias, horas, minutos e segundos
+#################################################################################################################################################################
 .data
 	num0: .word 60
 	num1: .word 3600
