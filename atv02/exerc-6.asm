4a494e93829f0067a304cdcba911db753713286b
JacquelinBusch <79542086+JacquelinBB@users.noreply.github.com>
2022-05-10 18:43:38 -0300

    Update exerc-6.asm

--- atv02/exerc-6.asm
+++ atv02/exerc-6.asm
@@ -1,6 +1,7 @@
 #################################################################################################################################################################
 #Nome do grupo: LearnWARE 
 #Questão de número: 6
+#Data: 07/05/2022
 #Atividade de número: 2
 #Disciplina: Infraestrutura de Hardware
 #Professor: Vitor Coutinho
