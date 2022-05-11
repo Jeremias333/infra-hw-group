6c63d4b6eec29e14c0e431762555d1fcc556e28f
Elder Lamarck <elder.lamarck@gmail.com>
2022-05-10 21:32:52 -0300

    Update exerc-7.asm

--- atv02/exerc-7.asm
+++ atv02/exerc-7.asm
@@ -10,29 +10,26 @@
 #Alunos: Diego Moura, Elder Lamarck, Jacquelin Busch, Jeremias Oliveira
 #Objetivo do algoritmo: Calcular a PG usando recursividade, recebendo dois input
 #################################################################################################################################################################
-.data
-	num0: .word 0
-
-	#->Razão q=$s1   ->Termo n da PG=$s2
 .text
+main:
 	# Pegar input (razão q) do usuário
 	li $v0, 5
 	syscall
 	
 	# Salvar o resultado em $s1
-	move $s1, $v0
+	move $s1, $v0 #valor de q
 	
 	# Pegar input (termo n da PG que deseja ser calculado) do usuário
 	li $v0, 5
 	syscall
 	
 	# Salvar o resultado em $s2
-	move $s2, $v0
+	move $s2, $v0 #valor de n
 	
 	#Receber o valor
-	lw $t1, num0 #recebe 0 em $t1
+	move $t1, $0 #recebe 0 em $t1
 	
-	#Chama a função calculopg (n)
+	#Chama a fun��o calculopg (n)
 	jal calculopg
 	
 	#Printar o valor calculo
@@ -40,23 +37,22 @@
 	move $a0, $s3 #alterar
 	syscall
 	
-	#Fim do programa
-	li $v0,10
-	syscall
-
+	j exit
+	
 calculopg:
 	sub $sp, $sp, 8
 	sw $ra, 4($sp)
 	sw $a0, 0($sp)
 	
 	slti $t0, $a0, 1
-	beq $t0, $zero, label
+	beq $t0, $0, label
 	
 	li $v0, 1
 	add $sp, $sp, 8
 	jr $ra
 	
-label:  sub $a0, $a0, 1
+label:  
+	sub $a0, $a0, 1
 	jal calculopg
 	
 	lw $a0, 0($sp)
@@ -65,3 +61,8 @@ label:  sub $a0, $a0, 1
 	
 	mul $v0, $a0, $v0
 	jr $ra
+
+exit:
+	#Fim do programa
+	li $v0,10
+	syscall
