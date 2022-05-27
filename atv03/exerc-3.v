f5a625027d51b27c8a4b1a1a7806e34d97a1d36f
JacquelinBusch <79542086+JacquelinBB@users.noreply.github.com>
2022-05-27 18:24:01 -0300

    Update exerc-3.v

--- atv03/exerc-3.v
+++ atv03/exerc-3.v
@@ -1,1 +1,12 @@
-
+#################################################################################################################################################################
+#Nome do grupo: LearnWARE 
+#Questão de número: 3
+#Data: 27/05/2022
+#Atividade de número: 3
+#Disciplina: Infraestrutura de Hardware
+#Professor: Vitor Coutinho
+#Semestre Letivo: 3º semestre
+#Turma: B
+#Alunos: Diego Moura, Elder Lamarck, Jacquelin Busch, Jeremias Oliveira
+#Objetivo do algoritmo: Vai pegar um input inteiro do usuário em segundos e transformar isso em um formato de anos, meses, dias, horas, minutos e segundos
+#################################################################################################################################################################
