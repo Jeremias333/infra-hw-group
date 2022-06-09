e265fe26fad49512287fd312cd1365b71ba8377f
JacquelinBusch <79542086+JacquelinBB@users.noreply.github.com>
2022-06-09 18:23:36 -0300

    Create exerc-1.v

---
+++ atv03/exerc-1.v
@@ -0,0 +1,12 @@
+/*
+-Nome do grupo: LearnWARE 
+-Questão de número: 2
+-Data: 09/06/2022
+-Atividade de número: 3
+-Disciplina: Infraestrutura de Hardware
+-Professor: Vitor Coutinho
+-Semestre Letivo: 3º semestre
+-Turma: B
+-Alunos: Paulo Guerreiro, Elder Lamarck, Jacquelin Busch, Jeremias Oliveira
+-Objetivo do algoritmo: É uma unidade lógico aritmética em verilog.
+*/
