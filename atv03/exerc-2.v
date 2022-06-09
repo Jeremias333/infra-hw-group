c6858411de21ba1e7b9635590104862b426b7d72
JacquelinBusch <79542086+JacquelinBB@users.noreply.github.com>
2022-06-09 19:32:19 -0300

    Update exerc-2.v

--- atv03/exerc-2.v
+++ atv03/exerc-2.v
@@ -18,16 +18,19 @@ module shiftreg (clk, data_in, data_out, shift_in, ctrl);
 	
 	//descrever comportamento
 	always @ (posedge clk) begin //2'b1=01; 2'b2=010; 2'b3=11
-		if (ctrl == 1'b0) begin
+		if (ctrl == 1'b00) begin
 			data_out <= data_out[0]; //mantém a saída (data_out) no estado atual ->NAO SEI SE TA CERTO
 		end
-		else if (ctrl == 2'b1) begin 
+		else if (ctrl == 2'b01) begin 
 			data_out <= data_in; //carrega os bits da entrada paralela para o registrador (data_out)
 		end
-		else begin
+		else if (ctrl == 2'b10) begin 
 			data_out <= data_out >> 1; //Deslocamento a direita
 			data_out[7]  <= shift_in;
 		end
-		//FALTA 1 o de 11
+		else begin
+			data_out <= data_out >> 1; 
+			data_out[6]  <= shift_in;
+		end
 	end
 endmodule 
