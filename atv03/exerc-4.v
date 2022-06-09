0a72c3e8ff374a5e25c84bbf3de211ea6b070c09
JacquelinBusch <79542086+JacquelinBB@users.noreply.github.com>
2022-06-09 19:00:29 -0300

    Update exerc-4.v

--- atv03/exerc-4.v
+++ atv03/exerc-4.v
@@ -1,3 +1,15 @@
+/*
+-Nome do grupo: LearnWARE 
+-Questão de número: 4
+-Data: 27/05/2022
+-Atividade de número: 3
+-Disciplina: Infraestrutura de Hardware
+-Professor: Vitor Coutinho
+-Semestre Letivo: 3º semestre
+-Turma: B
+-Alunos: Paulo Guerreiro, Elder Lamarck, Jacquelin Busch, Jeremias Oliveira
+-Objetivo do algoritmo: É uma multiplicar sem usar o operador de multiplicação em verilog.
+*/
 module mult #(parameter N=2)
 	(input start, rst, 
 	output reg [7:0] saida);
