72c383f33abc355f4ac5308251640e8a4907361b
JacquelinBusch <79542086+JacquelinBB@users.noreply.github.com>
2022-06-09 18:34:21 -0300

    Update exerc-1.v

--- atv03/exerc-1.v
+++ atv03/exerc-1.v
@@ -10,3 +10,19 @@
 -Alunos: Paulo Guerreiro, Elder Lamarck, Jacquelin Busch, Jeremias Oliveira
 -Objetivo do algoritmo: É uma unidade lógico aritmética em verilog.
 */
+module ula (a, b, r, f, status_d, ctrl_f);
+	input wire opcode, ctrl_f;
+	input wire [7:0] a, b;   
+	output reg [7:0] r;
+	outpu reg status_d;
+
+	always @(a or b or opcode) begin
+		case(opcode) //2'b1=01; 2'b2=010; 2'b3=11
+			2'b0: r = a + b;
+			2'b1: r = a - b;
+			2'b2: r = a << b;
+			2'b3: r = ~a || ~b;
+		endcase
+	end
+
+endmodule
